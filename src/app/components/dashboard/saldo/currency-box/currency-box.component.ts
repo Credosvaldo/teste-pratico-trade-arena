70d2568daf1fd011915e913a4c9d326dc0bcc74b
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2025-02-06 17:27:47 -0300

    alterando os valores no topo da pagina

--- src/app/components/dashboard/saldo/currency-box/currency-box.component.ts
+++ src/app/components/dashboard/saldo/currency-box/currency-box.component.ts
@@ -9,6 +9,6 @@ export class CurrencyBoxComponent {
   @Input({required: true}) name! : string;
   @Input({required: true}) image! : string;
   @Input({required: true}) unit! : string;
-  @Input({required: true}) valor! : number;
+  @Input({required: true}) valor! : string;
 
 }
